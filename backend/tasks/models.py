5d55bde027e8c9349c4812fbe1d676cd8e020e6f
ALBERT AMOAKO <orgin11phile@gmail.com>
2025-10-13 09:59:36 +0000

    Created the task model

---
+++ backend/tasks/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+# Create your models here.
+
+class Task(models.Model):
+  STATUS_PENDING = 'Pending'
+  STATUS_IN_PROGRESS = 'In Progress'
+  STATUS_COMPLETED = 'Completed'
+  STATUS_CHOICES = [
+      (STATUS_PENDING, 'Pending'),
+      (STATUS_IN_PROGRESS, 'In Progress'),
+      (STATUS_COMPLETED, 'Completed'),
+  ]
+  
+  PRIORITY_LOW = 'Low'
+  PRIORITY_MEDIUM = 'Medium'
+  PRIORITY_HIGH = 'High'
+  PRIORITY_CHOICES = [
+      (PRIORITY_LOW, 'Low'),
+      (PRIORITY_MEDIUM, 'Medium'),
+      (PRIORITY_HIGH, 'High'),
+  ]
+
+  title = models.CharField(max_length=255)
+  description = models.TextField()
+  #Change to ForeignKey to Account model after @clifforddonk pushes app
+  assigned_to = models.CharField(max_length=100)
+  #Change to ForeignKey to Account model after @clifforddonk pushes app
+  created_by = models.CharField(max_length=100)
+  status = models.CharField(max_length=20, choices=STATUS_CHOICES, default=STATUS_PENDING)
+  priority = models.CharField(max_length=10, choices=PRIORITY_CHOICES, default=PRIORITY_MEDIUM)
+  deadline = models.DateField(null=True, blank=True)
+  created_at = models.DateTimeField(auto_now_add=True)
+  
