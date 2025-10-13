e2ed69ebada923cfa0b6a61909a5177107db2434
ALBERT AMOAKO <orgin11phile@gmail.com>
2025-10-13 10:10:24 +0000

    Created the activity model

---
+++ backend/activity/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from tasks.models import Task
+# Create your models here.
+class Activity(models.Model):
+    action = models.CharField(max_length=255)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    user = models.CharField(max_length=100)  # Change to ForeignKey to Account model after @clifforddonk pushes app
+    related_task = models.ForeignKey(Task, on_delete=models.CASCADE, null=True, blank=True)
+    description = models.TextField(blank=True)
+    
+    class Meta:
+        ordering = ['-timestamp']
+    
+    def __str__(self):
+        return f"{self.user} - {self.action} at {self.timestamp}"
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    #middlewares
+    
+    
+    #apps
+    "tasks",
+    "activity",
+    
 ]
 
 MIDDLEWARE = [
