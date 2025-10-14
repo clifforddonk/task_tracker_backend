1ae617179153cc63c6c2c075aaa74ccab200b515
Clifford Aduako Donkor <clifforddonk@gmail.com>
2025-10-14 11:23:06 +0000

    Add created_by field to Task model and update user URLs to include UserListView

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import SignUpView, UserProfileView
+from .views import SignUpView, UserProfileView,UserListView
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
     path('login/', TokenObtainPairView.as_view(), name='login'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('profile/', UserProfileView.as_view(), name='profile'),
+    path('users/', UserListView.as_view(), name='user_list'),
 ]
