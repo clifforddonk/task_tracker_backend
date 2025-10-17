1294a6b4bc67dbdce8b82550ce86eb0b26c8f079
ALBERT AMOAKO <orgin11phile@gmail.com>
2025-10-17 12:44:22 +0000

    update axios instance to use production URL

--- frontend/axios/axiosInstance.js
+++ frontend/axios/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: "https://task-tracker-backend-m40u.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
@@ -38,7 +38,7 @@ axiosInstance.interceptors.response.use(
       if (refreshToken) {
         try {
           const response = await axios.post(
-            "http://127.0.0.1:8000/auth/token/refresh/",
+            "https://task-tracker-backend-m40u.onrender.com/auth/token/refresh/",
             { refresh: refreshToken }
           );
 
